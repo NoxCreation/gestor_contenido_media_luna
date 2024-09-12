0901ded3021f7ecffce6782042a63bea91d5b711
erickdevup <erickdevup@gmail.com>
2024-09-12 11:01:05 +0200

    add cambio moneda entity

---
+++ src/api/cambio-moneda-tienda/routes/cambio-moneda-tienda.ts
@@ -0,0 +1,7 @@
+/**
+ * cambio-moneda-tienda router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cambio-moneda-tienda.cambio-moneda-tienda');
