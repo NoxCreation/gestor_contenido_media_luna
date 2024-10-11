3bc8e8a63652af8bab00436e198274a77609b45f
erickdevup <erickdevup@gmail.com>
2024-10-11 13:38:16 +0200

    add colection retirar_ganancias_referidos

---
+++ src/api/retirar-ganancias-referido/controllers/retirar-ganancias-referido.ts
@@ -0,0 +1,7 @@
+/**
+ * retirar-ganancias-referido controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::retirar-ganancias-referido.retirar-ganancias-referido');
---
+++ src/api/retirar-ganancias-referido/services/retirar-ganancias-referido.ts
@@ -0,0 +1,7 @@
+/**
+ * retirar-ganancias-referido service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::retirar-ganancias-referido.retirar-ganancias-referido');
